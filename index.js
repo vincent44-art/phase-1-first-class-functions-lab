9bf405d9a74e71af12f9ff051e766f2020ad15fb
vincent44-art <vincent.murithi@student.moringaschoo.com>
2025-03-11 12:08:28 +0300

    commit

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+// Function to return the first two drivers
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+  };
+  
+  // Function to return the last two drivers
+  const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+  };
+  
+  // Array containing the two driver functions
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  
+  // Higher-order function that returns a fare multiplier function
+  const createFareMultiplier = function(multiplier) {
+    return function(fare) {
+      return fare * multiplier;
+    };
+  };
+  
+  // Fare doubler function using createFareMultiplier
+  const fareDoubler = createFareMultiplier(2);
+  
+  // Fare tripler function using createFareMultiplier
+  const fareTripler = createFareMultiplier(3);
+  
+  // Function to select different drivers
+  const selectDifferentDrivers = function(drivers, driverFunction) {
+    return driverFunction(drivers);
+  };
